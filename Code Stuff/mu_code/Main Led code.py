ab1a912baefaae3e332f2b293d2b46003d46a9a8
Terrien <45405194+TerrienDJV5@users.noreply.github.com>
2021-12-01 12:18:26 -0500

    Add files via upload

--- Code Stuff/mu_code/Main Led code.py
+++ Code Stuff/mu_code/Main Led code.py
@@ -15,32 +15,42 @@ GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(2.5) # Initialization
-try:
-  while True:
+
+servoRotation = [5,7.5,10,12.5,10,7.5,5,2.5]
+servoTiming = [0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5]
+setServoI = 0
+
+while True:
     led.on()
-    time.sleep(1)
+    #time.sleep(1)
     led.off()
-    time.sleep(1)
+    #time.sleep(1)
+    print(time.time())
 
-
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
-except KeyboardInterrupt:
-  p.stop()
-  GPIO.cleanup()
+    try:
+        p.ChangeDutyCycle(servoRotation[setServoI])
+        time.sleep(servoTiming[setServoI])
+        #p.ChangeDutyCycle(5)
+        #time.sleep(0.5)
+        #p.ChangeDutyCycle(7.5)
+        #time.sleep(0.5)
+        #p.ChangeDutyCycle(10)
+        #time.sleep(0.5)
+        #p.ChangeDutyCycle(12.5)
+        #time.sleep(0.5)
+        #p.ChangeDutyCycle(10)
+        #time.sleep(0.5)
+        #p.ChangeDutyCycle(7.5)
+        #time.sleep(0.5)
+        #p.ChangeDutyCycle(5)
+        #time.sleep(0.5)
+        #p.ChangeDutyCycle(2.5)
+        #time.sleep(0.5)
+        setServoI += 1
+        if setServoI>= len(servoRotation):
+            setServoI = 0
+    except KeyboardInterrupt:
+        p.stop()
+        GPIO.cleanup()
 
 
